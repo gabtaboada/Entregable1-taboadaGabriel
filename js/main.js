d17df2967e083f817494e63f701f969a805d6dcc
gabriel taboada <gababoada@yahoo.com.ar>
2026-01-21 15:42:34 -0300

    Proyecto solo con opcion crear pedidos. Opcion ver estado del pedido se realizara mas adelante

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 function CrearPedido(){
     let combos = []
-    let seleccion = parseInt(prompt("::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n4 => Volver al menu anterior"))
+    let seleccion = parseInt(prompt("::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n\n4 => Finalizar pedido "))
     while(seleccion !== 4){
         switch(seleccion){
             case 1 :
                 combos.push("CheeseBurger") 
+                alert("combo agregado")
                 break
             case 2 :
                 combos.push("BlueBurger")
+                alert("combo agregado")
                 break
             case 3 :
                 combos.push("VeganBurger")
+                alert("combo agregado")
                 break
             default : 
                 alert("Selección incorrecta")    
         }
-        if((seleccion > 0) && (seleccion <4))
-            seleccion = parseInt(prompt("¿Desea agregar otro combo a su pedido?\n::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n4 => Volver al menu anterior"))
-            else
-                seleccion = parseInt(prompt("::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n4 => Volver al menu anterior"))
+        if(combos.length)
+            {
+                seleccion = parseInt(prompt("¿Desea agregar otro combo a su pedido?\n::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n\n4 => Finalizar pedido"))
+            }
+            else{
+                seleccion = parseInt(prompt("::: Seleccione el combo :::\n\n1 => CheeseBurger :: $18000\n2 => BlueBurger :: $19500\n3 => VeganBurger :: $18750 \n\n4 => Finalizar pedido"))
+            }
+
     }
     return combos
 }
@@ -40,34 +47,39 @@ function ProcesarPedido(combos){
     }
     return importe
 }
-function MostrarPedido(combos,importe){
+function MostrarPedido(combos,importe,id){
     let mensaje =""
-    for (combo of combos){
-        mensaje += combo+"\n"
+    if( importe != 0){
+        let direccion = prompt("Ingrese su dirección")
+        mensaje += "Numero de pedido : "+id+"\n\n"
+        mensaje +="Combos seleccionados\n\n"
+        for (combo of combos){
+            mensaje += "* "+combo+"\n"
+        }
+        mensaje += "\n\n :::: IMPORTE TOTAL :::: \n$"+importe
+        mensaje += "\n\nDirección : "+direccion
     }
-    if( importe != 0)
-        mensaje += "\n :::: IMPORTE TOTAL :::: \n$"+importe
     else
         mensaje += "No selecciono ningun combo. Carrito vacio"
     alert(mensaje)
 }
+
 let combos = []
 let menu = parseInt(prompt("Ingrese 1 para hacer un nuevo pedido \nIngrese 2 para ver su pedido \nIngrese 3 para salir"))
-
+let importe = 0
+let Id = 1
 while ( menu !== 3 ){
     switch (menu){
         case 1 :
             combos = CrearPedido()
-            console.log("combos pedidos")
-            console.log(combos)
-            let importe = ProcesarPedido(combos)
-            console.log("importe total "+importe)
-            MostrarPedido(combos,importe)
+            importe = ProcesarPedido(combos)
+            MostrarPedido(combos,importe,Id)
+            if (importe != 0)
+                Id = Id + 1
             break
         case 2 :
-            let buscarPedido = parseInt(prompt("Ingrese el numero de pedido que desea buscar"))
+            alert ("Esta función estara disponible proximamente")
             
-            console.log("ver pedido")
             break
         default :
             alert("Opción incorrecta")
